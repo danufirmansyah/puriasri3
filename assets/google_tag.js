0e20b90b2831363b27036844446810318298c325
danufirmansyah <danufirmansyah25@gmail.com>
2024-12-12 06:13:47 +0700

    Changes :
    + add google_tag

---
+++ assets/google_tag.js
@@ -0,0 +1,13 @@
+// google-tag.js
+(function() {
+    var gtagScript = document.createElement('script');
+    gtagScript.async = true;
+    gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=AW-16803914945";
+    document.head.appendChild(gtagScript);
+  
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'AW-16803914945');
+  })();
+  
